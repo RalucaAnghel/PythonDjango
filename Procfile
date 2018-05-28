b62ccb50d512e23e95f16ca799c967f7beec7cac
RalucaAnghe <raluca.ionela.anghel@gmail.com>
2018-05-28 21:08:32 +0200

    potatoes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sudowork.wsgi
---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/raluca/Desktop/Development/sudowork/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
